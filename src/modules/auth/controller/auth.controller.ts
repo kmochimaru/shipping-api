4cc223d2136df2aea98aee7118c1521a29d0d8ca
Jirayu Mool-Ang <kmochimeru@gmail.com>
2021-02-26 22:21:54 +0700

    Add: auth/login set Public

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../service/auth.service';
 import { LocalAuthGuard } from './../jwt/local-auth.guard';
 import { Controller, Get, HttpCode, Post, Request, UseGuards, HttpStatus } from '@nestjs/common';
-import { JwtAuthGuard } from '../jwt/jwt-auth.guard';
+import { JwtAuthGuard, Public } from '../jwt/jwt-auth.guard';
 
 @Controller('api/v1/auth')
 export class AuthController {
@@ -10,6 +10,7 @@ export class AuthController {
         private _authService: AuthService
     ) { }
 
+    @Public()
     @UseGuards(LocalAuthGuard)
     @HttpCode(HttpStatus.OK)
     @Post('login')
